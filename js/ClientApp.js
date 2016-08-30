91e8d04aa4c4ef3479b5eed010cea6be17f30e45
rayGreycloud <rayGreycloud@gmail.com>
2016-08-29 18:04:59 -0700

    Add props to ClientApp

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -6,19 +6,21 @@ var MyTitle = React.createClass({
   render () {
     return (
       div(null,
-        h1(null, 'This is the second component')
+        h1(null, this.props.title)
       )
     )
   }
 })
 
 var MyTitleFact = React.createFactory(MyTitle)
+var ce = React.createElement
 
 var MyFirstComponent = (
   div(null,
-    MyTitleFact(null),
-    MyTitleFact(null),
-    MyTitleFact(null)
+    // 3 different ways to show instance of MyTitle
+    MyTitleFact({ title: 'Props are great!'}),
+    React.createElement(MyTitle, { title: 'Use props everywhere!'}),
+    ce(MyTitle, { title: 'Props are cool!'})
     // React.createElement(MyTitle, null)
   )
 )

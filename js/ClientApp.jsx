203bff8d3b834adebdb89aaa8fe8e9dafe6828a5
rayGreycloud <rayGreycloud@gmail.com>
2016-08-30 11:59:03 -0700

    Refactor ClientApp using ES6 syntax

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,14 +2,15 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const MyTitle = require('./MyTitle')
 
-const MyFirstComponent = function () {
-  return (
-    <div>
-      <MyTitle title="Title1" color="rebeccapurple" />
-      <MyTitle title="Title2" color="papaywhip" />
-      <MyTitle title="Title3" color="peru" />
-    </div>
-  )
-}
+// const MyFirstComponent = () => {
+//   return (
+const MyFirstComponent = () => (
+  <div>
+    <MyTitle title='Title1' color='rebeccapurple' />
+    <MyTitle title='Title2' color='papayawhip' />
+    <MyTitle title='Title3' color='peru' />
+  </div>
+)
+//  )}
 
 ReactDOM.render(<MyFirstComponent />, document.getElementById('app'))

01e0238640c223924d534be28ac57389cd261871
rayGreycloud <rayGreycloud@gmail.com>
2016-08-31 14:24:35 -0700

    Refactor Search with state and add data binding to search input

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,14 +2,30 @@ const React = require('react')
 const ShowCard = require('./ShowCard')
 const data = require('../public/data')
 
-const Search = () => (
-  <div className='container'>
-    <div className='shows'>
-      {data.shows.map((show) => (
-        <ShowCard key={show.imdbID} {...show} />
-      ))}
-    </div>
-  </div>
-)
+const Search = React.createClass({
+  getInitialState() {
+    return {
+      searchTerm: 'this is the search term'
+    }
+  },
+  handleSearchTermEvent(event) {
+    this.setState({ searchTerm: event.target.value})
+  },
+  render() {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>sVideo</h1>
+          <input value={this.state.searchTerm} className='search-input' type='text' placeholder='Search' onChange={this.handleSearchTermEvent} />
+        </header>
+        <div className='shows'>
+          {data.shows.map((show) => (
+            <ShowCard key={show.imdbID} {...show} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+})
 
 module.exports = Search

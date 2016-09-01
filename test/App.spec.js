8497b4c6f6a1bf83d033296087f77bb2de63f648
rayGreycloud <rayGreycloud@gmail.com>
2016-09-01 08:38:34 -0700

    Add spec to test search filtering

--- test/App.spec.js
+++ test/App.spec.js
@@ -4,7 +4,7 @@ const { expect } = require('chai')
 const React = require('react')
 const Search = require('../js/Search')
 const ShowCard = require('../js/ShowCard')
-const { shallow } = require('enzyme')
+const { shallow, mount } = require('enzyme')
 const { shows } = require('../public/data')
 
 describe('<Search />', () => {
@@ -16,5 +16,14 @@ describe('<Search />', () => {
   it('should render show for all data', () => {
     const wrapper = shallow(<Search />)
     expect(wrapper.find(ShowCard).length).to.equal(shows.length)
+  })
+
+  it('should filter correctly given new state',  () => {
+    const wrapper = mount(<Search />)
+    const input = wrapper.find('.search-input')
+    input.node.value = 'house'
+    input.simulate('change')
+    expect(wrapper.state('searchTerm')).to.equal('house')
+    expect(wrapper.find('.show-card').length).to.equal(2)
   })
 })

8d856d8015aac6bdb48d6f3f5949103f21987605
rayGreycloud <rayGreycloud@gmail.com>
2016-09-01 16:53:34 -0700

    Add assignShow method to App and search route

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -8,14 +8,29 @@ const { shows } = require('../public/data')
 
 const { Router, Route, IndexRoute, hashHistory } = require('react-router')
 
-const App = () => (
-  <Router history={hashHistory}>
-    <Route path='/' component={Layout}>
-      <IndexRoute component={Landing} />
-      <Route path='/search' component={Search} shows={shows} />
-      <Route path='/details/:id' component={Details} />
-    </Route>
-  </Router>
-)
+const App = React.createClass({
+  assignShow (nextState, replace) {
+    const showArray = shows.filter(show => show.imdbID === nextState.id)
+
+    if (showArray.length < 1) {
+      return replace('/')
+    }
+
+    Object.assign(nextState.params, showArray[0])
+    return nextState
+  },
+  render () {
+    return (
+      <Router history={hashHistory}>
+        <Route path='/' component={Layout}>
+          <IndexRoute component={Landing} />
+          <Route path='/search' component={Search} shows={shows} />
+          <Route path='/details/:id' component={Details}
+            onEnter={this.assignShow} />
+        </Route>
+      </Router>
+    )
+  }
+})
 
 ReactDOM.render(<App />, document.getElementById('app'))

773d8371cf0a87bbd39ee9a1c95c63b01b3bc6fb
rayGreycloud <rayGreycloud@gmail.com>
2016-09-01 16:17:41 -0700

    Pass data in ClientApp to search route

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -4,6 +4,7 @@ const Landing = require('./Landing')
 const Search = require('./Search')
 const Layout = require('./Layout')
 const Details = require('./Details')
+const { shows } = require('../public/data')
 
 const { Router, Route, IndexRoute, hashHistory } = require('react-router')
 
@@ -11,7 +12,7 @@ const App = () => (
   <Router history={hashHistory}>
     <Route path='/' component={Layout}>
       <IndexRoute component={Landing} />
-      <Route path='/search' component={Search} />
+      <Route path='/search' component={Search} shows={shows} />
       <Route path='/details/:id' component={Details} />
     </Route>
   </Router>

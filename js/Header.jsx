2d6411af3202e949fe8239ff55a8658bfa468ace
rayGreycloud <rayGreycloud@gmail.com>
2016-09-02 12:10:55 -0700

    Add Header to search page and refactor onChange function

--- js/Header.jsx
+++ js/Header.jsx
@@ -1,8 +1,27 @@
 const React = require('react')
 const { Link } = require('react-router')
+const { func, bool, string } = React.PropTypes
 
 const Header = React.createClass({
+  propTypes: {
+    handleSearchTermChange: func,
+    showSearch: bool,
+    searchTerm: string
+  },
+  handleSearchTermEvent (event) {
+    this.props.handleSearchTermChange(event.target.value)
+  },
   render () {
+    let utilSpace
+    if (this.props.showSearch) {
+      utilSpace = <input type='text' className='search-input' placeholder='search' value={this.props.searchTerm} onChange={this.handleSearchTermEvent} />
+    } else {
+      utilSpace = (
+        <h2 className='header-back'>
+          <Link to='/search'>Back</Link>
+        </h2>
+      )
+    }
     return (
       <header className='header'>
         <h1 className='brand'>
@@ -10,9 +29,7 @@ const Header = React.createClass({
             sVideo
           </Link>
         </h1>
-        <h2 className='header-back'>
-          <Link to='/search'>Back</Link>
-        </h2>
+        {utilSpace}
       </header>
     )
   }
